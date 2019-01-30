b6c6fb87187929afc9108b31eab63d76059c25c8
Ola Podsiadlik <alecs@op.pl>
2019-01-30 19:32:13 +0100

    Finish task

---
+++ program.js
@@ -0,0 +1,25 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    var instruction = input.toString().trim();
+    
+    
+    switch (instruction) {
+                
+        case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+        
+        case '/system':
+        process.stdout.write('Wersja językowa: ' + process.env.LANG + '\nWersja Node: ' + process.versions.node + '\n');
+        process.exit();
+        break;
+  
+        default:
+        process.stdout.write('Wrong instruction!\n');
+        
+}
+});
