794696643ac69bc2a322e5bf70796d31155e9d84
Ola Podsiadlik <alecs@op.pl>
2019-01-30 20:02:24 +0100

    Final version

--- program.js
+++ program.js
@@ -3,23 +3,25 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
-    var instruction = input.toString().trim();
     
+    if(input !== null) {
+        var instruction = input.trim();
     
-    switch (instruction) {
+        switch (instruction) {
                 
-        case '/exit':
-        process.stdout.write('Quitting app!\n');
-        process.exit();
-        break;
+            case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
         
-        case '/system':
-        process.stdout.write('Wersja językowa: ' + process.env.LANG + '\nWersja Node: ' + process.versions.node + '\n');
-        process.exit();
-        break;
+            case '/system':
+            process.stdout.write('Wersja językowa: ' + process.env.LANG + '\nWersja Node: ' + process.versions.node + '\n');
+            process.exit();
+            break;
   
-        default:
-        process.stdout.write('Wrong instruction!\n');
+            default:
+            process.stdout.write('Wrong instruction!\n');
         
-}
+        };
+    };
 });
